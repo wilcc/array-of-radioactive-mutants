a2e318b79fee8992a59377854080e2b9c13cdd51
Wilson Chen <wilson.chen@codeimmersives.com>
2020-02-24 14:52:40 -0500

    finish

--- main.js
+++ main.js
@@ -2,10 +2,41 @@
  * YOUR CODE HERE *
  ******************/
 
+function changeLast(array,newValue){
+  array.pop()
+  array.push(newValue)
+}
+function addMeToEnd(array){
+  array.push('Colin')
+}
+function addMeToStart(array){
+  array.unshift('Colin')
+}
+function changeAllValuesTo(array,value){
+  
+  for (let i = 0; i < array.length ; i++){
+    array.shift()
+    array.push(value)
+  }
+}
 
 
+function oddOrEven(array){
+  for (let i = 0; i < array.length ; i++){
+    if (array[i] % 2 === 0){
+      array[i] = 'even'
+      
+    }else array[i]= 'odd'
+}}
 
+function changeNextThreeToValue(start,arr,value){
+  arr.splice(start,3,value,value,value)
+  
+}
 
+// function changeAllValuesTo(){
+  
+// }
 /*********************************
  * OUR CODE BELOW; DO NOT TOUCH! *
  *********************************/
